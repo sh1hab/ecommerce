a0aa5598fc72a4a8a00538bd05bc361c5da132c1
sh1hab <shihab.gsc10@gmail.com>
2019-12-10 00:36:45 +0600

    added cart controller

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Dotenv\Exception\ValidationException;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -23,22 +24,26 @@ class CartController extends Controller
         $product = Product::findOrFail( $request->input('product_id') );
 
         $cart = session()->get('cart') ?? [];
-
+        //dd($cart);
         if( session()->has('cart') )
         {
-            if ( array_key_exists($product->id,$cart) )
+            var_dump($product->id);
+            if ( array_key_exists($product->id,$cart['products'] ) )
             {
+                var_dump('here');
                 $cart['products'][$product->id]['quantity'] += 1;
-            }else{
-                $cart['products'] = [
+            }
+            else{
+                array_push( $cart['products'], [
                     $product->id => [
                         'title'     =>  $product->title,
                         'quantity'  =>  1,
                         'price'     =>  $product->sale_price ?? $product->price
                     ]
-                ];
+                    ] );
             }
         }else{
+//            var_dump('new cart');
             $cart['products'] = [
                 $product->id => [
                     'title'     =>  $product->title,
@@ -50,6 +55,8 @@ class CartController extends Controller
 
         session(['cart'=>$cart]);
 
+        dd( session()->get('cart') );
+
         return redirect()->route('home');
 
 //        dd(session('cart'));
@@ -58,6 +65,9 @@ class CartController extends Controller
 
     function  showCart()
     {
+        session()->flush();
+
+        return redirect()->route('home');
 
     }
 }
